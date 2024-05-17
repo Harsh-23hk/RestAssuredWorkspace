c14bf31161a4c9326526a04b1f61235341fac797
Harsh <harshhk608@gmail.com>
2024-05-17 21:25:59 +0530

    jsonSchema

--- src/test/java/com/restassuredworkspace/Test/JsonValidation/JsonValidationSchema.java
+++ src/test/java/com/restassuredworkspace/Test/JsonValidation/JsonValidationSchema.java
@@ -1,11 +1,43 @@
 package com.restassuredworkspace.Test.JsonValidation;
 
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 public class JsonValidationSchema {
 
     @Test
     public void testValidationSchema() {
+
+        //https:restful-booker.herokuapp.com/auth
+
+        String payload = "{\n" +
+                "    \"username\" : \"admin\",\n" +
+                "    \"password\" : \"password123\"\n" +
+                "}";
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com/auth")
+                .contentType("application/json")
+                .body(payload)
+                .when().post()
+                .then()
+                .statusCode(200)
+                .body("token", Matchers.notNullValue())
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("Schema.json"))
+
+                       // matchesJsonSchema(new File("C:\\Users\\Harsh\\Desktop\\schema.json")))
+                ;
+
+
+
+
+
+
 
     }
 }
--- src/test/java/com/restassuredworkspace/Test/JsonValidation/ValidationWithoutRest.java
+++ src/test/java/com/restassuredworkspace/Test/JsonValidation/ValidationWithoutRest.java
@@ -1,2 +1,28 @@
-package com.restassuredworkspace.Test.JsonValidation;public class ValidationWithoutRest {
+package com.restassuredworkspace.Test.JsonValidation;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+public class ValidationWithoutRest {
+
+
+    @Test
+    public void testMethod(){
+
+        String json = "{\r\n"
+                + "        \"id\": 2,\r\n"
+                + "        \"email\": \"janet.weaver@reqres.in\",\r\n"
+                + "        \"first_name\": \"Janet\",\r\n"
+                + "        \"last_name\": \"Weaver\"\r\n"
+                + "}";
+
+        MatcherAssert.assertThat(json, JsonSchemaValidator.matchesJsonSchemaInClasspath("EmpSchema.json"));
+
+
+
+
+
+    }
 }
