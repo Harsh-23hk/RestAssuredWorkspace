930bfcededbaef8b37600f212a0c84f1dae0ff74
Harsh <harshhk608@gmail.com>
2024-05-18 21:05:50 +0530

    change oauth

--- src/test/java/com/restassuredworkspace/Test/Authincation/Oauth/OAuth.java
+++ src/test/java/com/restassuredworkspace/Test/Authincation/Oauth/OAuth.java
@@ -13,13 +13,6 @@ public class OAuth {
     public void oauth() {
 
         RequestSpecification request = RestAssured.given();
-        request.baseUri("https://api-m.sandbox.paypal.com")
-                .basePath("/v1/oauth2/token");
-
-
-        String client_Id = "AUXp0L9IVHrkyaoMcq0WQkrVBt-XC4N4PtTXMMRiXCnttvM1tG9yLU8pQoAynLpXT7VYXVKPYyBgSjtD";
-        String client_secret = "EAMxqnyL9IF4iYFj-25Yrf4tZ3EMoc6giruFlPnJYSVO4HLWTiQ18sw03YlGqNSoFqmOsmLgW4tszs-n";
-
 
         request.auth().preemptive().basic(client_Id, client_secret)
                 .param("grant_type", "client_credentials");
@@ -44,7 +37,7 @@ public class OAuth {
                 .queryParam("page", 3)
                 .queryParam("page_size", 4)
                 .queryParam("total_count_required", true)
-                .get("https://api-m.sandbox.paypal.com/v1/invoicing/invoices");
+
 
         response.prettyPrint();
 
